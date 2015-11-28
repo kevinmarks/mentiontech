d87d1e2b8a27109f7602c11c80285269993d537d
Kevin Marks <kevinmarks@gmail.com>
2015-11-27 22:22:03 -0800

    listmentions

--- main.py
+++ main.py
@@ -120,9 +120,23 @@ class VerifyMention(webapp2.RequestHandler):
             else:
                 logging.info("VerifyMention could not fetch %s to check for %s" % (mention.source,mention.target))
           
+class ListMentions(webapp2.RequestHandler):
+    def get(self):
+        target,targetdomain= geturlanddomain(self.request.get('target'))
+        targetkey = ndb.Key('Domain', targetdomain)
+        mentionquery = Mention.query(ancestor = targetkey).filter(Mention.targetdomain==targetdomain).order(-Mention.updated)
+        mentions = mentionquery.fetch(20)
+        for mention in mentions:
+            mention.humancreated = humanize.naturaltime(mention.created)
+            mention.humanupdated = humanize.naturaltime(mention.updated)
+        template_values={'mentions':mentions}
+        
+        template = JINJA_ENVIRONMENT.get_template(page+'.html')
+        self.response.write(template.render(template_values))
 
 app = webapp2.WSGIApplication([
     ('/webmention', WebmentionHandler),
     ('/verifymention/([^/]+)?', VerifyMention),
+    ('listmentions',ListMentions),
     ('/([^/]+)?', MainHandler),
 ], debug=True)
